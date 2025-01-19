4a3e97f21e7a388af6dfbb0d86c4117aa19ef52b
unknown <luna.colombanfernandez@ynov.com>
2025-01-19 16:53:12 +0100

    modification echelle map et tuiles

--- test.py
+++ test.py
@@ -7,10 +7,17 @@ pygame.init()
 # Charger la carte TMX
 tmx_data = pytmx.load_pygame('Assets/assets tiled/mapv2.tmx')
 
-# Déterminer la taille de la fenêtre en fonction de la carte
+# Vérifier la taille de la carte avant d'essayer d'afficher
 map_width = tmx_data.width * tmx_data.tilewidth
 map_height = tmx_data.height * tmx_data.tileheight
-screen = pygame.display.set_mode((map_width, map_height))
+
+# Si la taille de la carte est valide, initialiser l'écran
+if map_width > 0 and map_height > 0:
+    screen = pygame.display.set_mode((map_width, map_height))
+else:
+    print("Erreur: Dimensions de la carte non valides.")
+    pygame.quit()
+    exit()
 
 # Boucle principale du jeu
 running = True
